601a50939ffafdda60f01a04688e10a2aba128a2
Даниил <kourin.daniil@gmail.com>
2022-04-26 09:30:00 +0300

    Ekzamen

---
+++ ClassLibrary12/Application.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary12
+{
+    public class Application
+    {
+        public Application application;
+        public List<TaskList> taskLists;
+
+        public Application()
+        {
+            taskLists = new List<TaskList>();
+        }
+
+        /// <summary>
+        /// Метод получения экземпляра приложения
+        /// </summary>
+        
+        public Application GetApplication()
+        {
+            if (application == null)
+                return new Application();
+
+            return application;
+        }
+
+        /// <summary>
+        /// Создает список задач с указанным именем и добавляет его в taskLists.
+        /// </summary>
+        /// <param name="name"></param>
+        public void CreateTaskList(string name)
+        {
+            taskLists.Add(new TaskList(name));
+        }
+
+        /// <summary>
+        /// Возвращает список задач по имени списка.
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetTaskListNames()
+        {
+            if (taskLists == null)
+                return null;
+
+            List<string> taskListNames = new List<string>();
+
+            foreach (TaskList task in taskLists)
+            {
+                taskListNames.Add(task.Name);
+            }
+
+            return taskListNames;
+        }
+        /// <summary>
+        /// Метод для получения списка задач на завтра и позднее
+        /// </summary>
+        /// <returns></returns>
+        public List<Task> GetTasksByFuture()
+        {
+            List<Task> tasksByFuture = new List<Task>();
+
+            foreach (TaskList taskList in taskLists)
+            {
+                tasksByFuture.AddRange(taskList.GetTasksByFuture());
+            }
+
+            return tasksByFuture;
+        }
+        /// <summary>
+        /// Метод получения задач на сегодня
+        /// </summary>
+        /// <returns></returns>
+        public List<Task> GetTasksByToday()
+        {
+            List<Task> tasksByToday = new List<Task>();
+
+            foreach (TaskList taskList in taskLists)
+            {
+                tasksByToday.AddRange(taskList.GetTasksByToday());
+            }
+
+            return tasksByToday;
+        }
+
+    }
+
+}
---
+++ ClassLibrary12/TaskList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary12
+{
+    public class TaskList
+    {
+        public string Name { get; set; }
+        public List<Task> Tasks { get; set; }
+
+        public TaskList(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Метод получения имени списка задач
+        /// </summary>
+        /// <returns></returns>
+        public string GetName()
+        {
+            return Name;
+        }
+
+        /// <summary>
+        /// Метод удаления задачи из списка задач
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public void Remove(Task task)
+        {
+            if (Tasks.Contains(task))
+            {
+                Tasks.Remove(task);
+            }
+        }
+
+        /// <summary>
+        /// Метод получения задач списка задач
+        /// </summary>
+        /// <returns></returns>
+        public List<Task> GetTasks()
+        {
+            return Tasks;
+        }
+
+        /// <summary>
+        /// Метод получения задач на сегодня
+        /// </summary>
+        /// <returns></returns>
+        public List<Task> GetTasksByToday()
+        {
+            List<Task> tasksByToday = new List<Task>();
+
+            foreach (Task task in Tasks)
+            {
+                if (task.Due.ToShortDateString() == DateTime.Now.ToShortDateString())
+                {
+                    tasksByToday.Add(task);
+                }
+            }
+
+            return tasksByToday;
+        }
+        /// <summary>
+        /// Метод получения задач на сегодня и позднее
+        /// </summary>
+        /// <returns></returns>
+        public List<Task> GetTasksByFuture()
+        {
+            List<Task> tasksByFuture = new List<Task>();
+
+            foreach (Task task in Tasks)
+            {
+                if (task.Due > DateTime.Now)
+                {
+                    tasksByFuture.Add(task);
+                }
+            }
+
+            return tasksByFuture;
+        }
+
+        /// <summary>
+        /// Метод добавления задачи в список задач
+        /// </summary>
+        /// <param name="task">Задача</param>
+        public void AddTask(Task task)
+        {
+            Tasks.Add(task);
+        }
+    }
+
+}
+
